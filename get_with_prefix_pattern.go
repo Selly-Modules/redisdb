562d8c6f642c4676b617aff33a9fbbea0f8b7f86
Minh Nguyen <minh.milano1511@gmail.com>
2022-03-25 10:53:25 +0700

    get-data-by-pattern

---
+++ get_with_prefix_pattern.go
@@ -0,0 +1,33 @@
+package redisdb
+
+import "context"
+
+// GetWithPrefixPattern ...
+func GetWithPrefixPattern(pattern string, limit int64) (keys []string, values []string) {
+	// Init
+	keys = make([]string, 0)
+	values = make([]string, 0)
+
+	var (
+		ctx    = context.Background()
+		cursor uint64
+		err    error
+	)
+
+	keys, cursor, err = rdb.Scan(ctx, cursor, pattern, limit).Result()
+	if err != nil {
+		return
+	}
+
+	if len(keys) == 0 {
+		return
+	}
+
+	// Get value
+	for _, key := range keys {
+		value := GetValueByKey(ctx, key)
+		values = append(values, value)
+	}
+
+	return
+}
